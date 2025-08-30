3b06a916f8f83b65f694ac1a13a6b0fd766367ba
ZackPedretti <zacklycee.bp@gmail.com>
2025-08-30 15:21:14 +0200

    chore: :fire: Removed unused legacy Vue project

--- experitone-frontend/src/stores/youtube-player.ts
+++
@@ -1,7 +0,0 @@
-import type { Player } from '@vue-youtube/component'
-
-export const useYoutubePlayerStore = defineStore('youtube-player', () => {
-  const player = ref<Player | null>(null)
-
-  return { player }
-})
--- experitone-frontend/src/stores/youtube-video.ts
+++
@@ -1,6 +0,0 @@
-export const useYoutubeVideoStore = defineStore('youtube-video', () => {
-  const videoId = ref('26gDUjMxgXw')
-  const videoDuration = ref(0)
-
-  return { videoId, videoDuration }
-})
