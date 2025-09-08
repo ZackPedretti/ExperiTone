e172acf4dea4cc64d49a5c718ac0ea9576475be0
ZackPedretti <zacklycee.bp@gmail.com>
2025-09-08 17:30:32 +0200

    feat: :sparkles: Clickable timestamps for start and end of annotation

--- experitone-app/app/components/AnnotationCard.vue
+++ experitone-app/app/components/AnnotationCard.vue
@@ -1,22 +1,28 @@
 <script setup lang="ts">
-import type {Annotation} from "~/entities/Annotation";
+import type {Annotation} from "~~/entities/Annotation";
 
-const { t } = useI18n();
+const {t} = useI18n();
 
 const props = defineProps<{
   annotation: Annotation,
 }
 >()
 
-const author = await $fetch<{name: string, image: string}>(`/api/users/get-by-id?userId=${props.annotation.authorId}`)
+const author = await $fetch<{ name: string, image: string }>(`/api/users/get-by-id?userId=${props.annotation.authorId}`)
 const defaultPicture = "/img/default.png"
 
-const { player } = storeToRefs(useYoutubePlayerStore());
+const {player} = storeToRefs(useYoutubePlayerStore());
 
-function playFromAnnotationStart(){
-  player.value?.seekTo(props.annotation.startTimestamp)
+function playFrom(seconds: number) {
+  player.value?.seekTo(seconds)
 }
 
+function formatSecondsToMinutes(seconds: number): string {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  const paddedSeconds = remainingSeconds.toString().padStart(2, '0');
+  return `${minutes}:${paddedSeconds}`;
+}
 </script>
 
 <template>
@@ -38,7 +44,16 @@ function playFromAnnotationStart(){
           </v-btn>
         </v-row>
       </v-col>
-      <v-card-text>{{ annotation.description ?? '' }}</v-card-text>
+    </v-row>
+    <v-row>
+      <v-card-text class="font-italic pb-0 d-flex align-center justify-start ga-1 timestamp-container">
+        <span class="timestamp" @click="playFrom(annotation.startTimestamp)">{{ formatSecondsToMinutes(annotation.startTimestamp) }}</span>
+        <span v-if="annotation.endTimestamp" class="timestamp-dash"> - </span>
+        <span v-if="annotation.endTimestamp" class="timestamp" @click="playFrom(annotation.endTimestamp)">{{ formatSecondsToMinutes(annotation.endTimestamp) }}</span>
+      </v-card-text>
+    </v-row>
+    <v-row>
+      <v-card-text class="pt-0">{{ annotation.description ?? '' }}</v-card-text>
     </v-row>
     <v-row class="ml-4 mb-1" align="center">
       <v-avatar size="32">
@@ -47,10 +62,31 @@ function playFromAnnotationStart(){
       <v-card-text>{{ author.name }}</v-card-text>
     </v-row>
     <v-card-actions>
-      <v-btn class="text-body-2" prepend-icon="mdi-play" @click="playFromAnnotationStart"> {{ t('song-page.annotation-card.play-btn') }}</v-btn>
+      <v-btn class="text-body-2" prepend-icon="mdi-play" @click="playFrom(annotation.startTimestamp)">
+        {{ t('song-page.annotation-card.play-btn') }}
+      </v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.timestamp-container {
+  width: 40px;
+}
+
+.timestamp {
+  font-weight: normal;
+  transition: font-weight 0.1s;
+  width: 32px;
+
+  &:hover {
+    font-weight: bold;
+  }
+}
+
+.timestamp-dash {
+  width: 10px;
+  text-align: center;
+}
+
 </style>
